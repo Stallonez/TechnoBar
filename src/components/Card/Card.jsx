eb85c85c4f6bea09b8dc43b1144e119c758e2732
MaksimBabkov <mvc.software.engineer@gmail.com>
2022-05-24 12:07:58 +0300

    Исправил цену

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,6 +2,12 @@ import './Card.css';
 
 const Card = (props) => {
 
+    let getStatePrice = (price) => {
+        let numberPrice = Number(price)
+        let result = numberPrice + (numberPrice / 100) * 10
+        return result
+    }
+
     return (
         <div className="mainCard">
             <div className='creditInfo_flex_container'>
@@ -25,7 +31,7 @@ const Card = (props) => {
             </div>
             <div className='priceInfo'>
                 <div className='priceInfo__currentPrice'>{props.item.price}</div>
-                <div className='priceInfo__discountPrice'>{props.item.price}</div>
+                <div className='priceInfo__discountPrice'>{getStatePrice(props.item.price)}</div>
             </div>
         </div>
     )
--- src/redux/notebookData-reducer.js
+++ src/redux/notebookData-reducer.js
@@ -1,8 +1,9 @@
 let initialState = {
     notebook_Asus_Data: [
-        { id: 1, name: 'Ноутбук Asus X409FA i3 10110U', price: '249 990', img: "https://www.technodom.kz/media/catalog/product/cache/79d62b5a17d20b122cb8cbf050cc7241/a/f/afa67d9a20d0632cb2c6c2ea191166e14e45c020_249088_1.jpg" },
-        { id: 2, name: 'Ноутбук Asus VivoBook X413EA', price: '254 990', img: "https://www.technodom.kz/media/catalog/product/cache/1366e688ed42c99cd6439df4031862c6/8/d/8ddbb7576ee4763233f2274e2aaa4ae360da2d36_251232_3.jpg" },
-        { id: 3, name: 'Ноутбук Asus E510MA N5030', price: '229 990', img: "https://www.technodom.kz/media/catalog/product/cache/1366e688ed42c99cd6439df4031862c6/f/3/f3faaf1014f3ca85dba403368a98a727e06d1b62_250567_1.jpg" },
+        { id: 1, name: 'Ноутбук Asus X409FA i3 10110U', price: '249990', img: "https://www.technodom.kz/media/catalog/product/cache/79d62b5a17d20b122cb8cbf050cc7241/a/f/afa67d9a20d0632cb2c6c2ea191166e14e45c020_249088_1.jpg" },
+        { id: 2, name: 'Ноутбук Asus VivoBook X413EA', price: '254990', img: "https://www.technodom.kz/media/catalog/product/cache/1366e688ed42c99cd6439df4031862c6/8/d/8ddbb7576ee4763233f2274e2aaa4ae360da2d36_251232_3.jpg" },
+        { id: 3, name: 'Ноутбук Asus E510MA N5030', price: '229990', img: "https://www.technodom.kz/media/catalog/product/cache/1366e688ed42c99cd6439df4031862c6/f/3/f3faaf1014f3ca85dba403368a98a727e06d1b62_250567_1.jpg" },
+        { id: 4, name: 'Ноутбук Asus E510MA N5030', price: '229590', img: "https://www.technodom.kz/media/catalog/product/cache/1366e688ed42c99cd6439df4031862c6/f/3/f3faaf1014f3ca85dba403368a98a727e06d1b62_250567_1.jpg" }
     ],
     notebook_Lenovo_Data: [
         { id: 1, name: 'Ноутбук Lenovo V14 Athlon Gold 3150U / 8ГБ / 128SSD / 14 / Win10 / (82C6S03900)', price: '189 990 ₸', img: "https://www.technodom.kz/media/catalog/product/cache/1366e688ed42c99cd6439df4031862c6/9/1/917c7f0d8e4b6ce9b7a1e9145e3768f98369acc9_231330_8.jpg" },
